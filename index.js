cb15fcea9a4527446f4dd26536899ddf8ec1dfae
Arda Koç <ardaskoc@gmail.com>
2023-06-05 11:10:43 +0300

    rendered entered inputs.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function saveInput() {
-  console.log("Input saved...")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn?.addEventListener("click", function() {
+  renderInput(inputEl.value)
+  inputEl.value = ""
+})
+
+
+function renderInput(input) {
+  ulEl.innerHTML += "<li>" + input + "</li>"
 }
