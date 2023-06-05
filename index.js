6d834c4dca0f9755355643de7edb25ce93b8bf8b
Arda Koç <ardaskoc@gmail.com>
2023-06-05 15:20:41 +0300

    inputs are saved in local storage.

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+let myInputs = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let inputsFromLocalStorage = JSON.parse(localStorage.getItem("myInputs"))
+
+if (inputsFromLocalStorage) {
+  myInputs = inputsFromLocalStorage
+  renderInputs()
+}
 
 inputBtn?.addEventListener("click", function() {
-  renderInput(inputEl.value)
+  myInputs.push(inputEl.value)
   inputEl.value = ""
+  localStorage.setItem("myInputs", JSON.stringify(myInputs))
+  renderInputs()
 })
 
 
-function renderInput(input) {
-  ulEl.innerHTML += "<li>" + input + "</li>"
+function renderInputs() {
+  let listItems = ""
+  for (let i=0; i<myInputs.length; i++) {
+    listItems += `<li>${myInputs[i]}</li>`
+  }
+  ulEl.innerHTML = listItems
 }
